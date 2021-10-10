ed97db8a7a55f0b90896be93e252f3a096ec08e8
user <user@MacBook-Air-.local>
2021-10-10 12:11:32 +0300

    Lab Enum is Done

---
+++ Enums.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+enum Creature {
+    case Human
+    case Vampire
+    case Werewolf
+    
+}
+
+let myCreature = Creature.Vampire
+
+switch myCreature {
+case.Human:
+    print("i am a Human")
+case.Vampire:
+    print("i am a Vampire")
+case.Werewolf:
+    print(" i am a Werewolf")
+}
