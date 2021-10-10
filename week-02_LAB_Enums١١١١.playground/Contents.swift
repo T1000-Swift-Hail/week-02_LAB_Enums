2760f008bfcbcf9d17ab5a6a0733e57728c1b8b9
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-10 12:22:49 +0300

    solvee

---
+++ week-02_LAB_Enums١١١١.playground/Contents.swift
@@ -0,0 +1,17 @@
+enum Creature{
+    case Human
+    case Vampire
+    case Werewolf
+    
+}
+    let myCreature = Creature.Vampire
+    
+
+switch myCreature {
+case .Human:
+        print("I am a Human" )
+case .Vampire :
+        print("I am a Vampire")
+    case .Werewolf :
+        print( "I am a Werewolf")
+}
