c5f4eb3b49c0b37a443339813a71834b2b4894ff
mac <mac@MacBook-Pro-alkhas-b-mac.local>
2021-10-10 13:08:34 +0300

    finash

---
+++ week-02_LAB_Enums.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+enum Creature {
+  case Human
+  case Vampire
+  case Werewolf
+}
+let MyCreature = Creature.Vampire
+switch MyCreature {
+case .Vampire :
+  print(" I am Vampire ")
+case .Human :
+  print("I am Human ")
+case .Werewolf :
+  print( "I am Werewolf ")
+}
