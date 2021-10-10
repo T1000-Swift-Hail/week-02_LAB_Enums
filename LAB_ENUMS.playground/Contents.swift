545f9985051ba0795d3660d3574d8ba4112c0bdf
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-10 13:08:09 +0300

    done

---
+++ LAB_ENUMS.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+enum creature {
+    case Human
+    case Vampire
+    case werewolf
+}
+
+let mycreature = creature.Vampire
+switch mycreature{
+case.Human:
+print ("Iam a Human")
+case.Vampire:
+print("I am a vampire")
+case.werewolf:
+    print("I am a werewolf")
+}
