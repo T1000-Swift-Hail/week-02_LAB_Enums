bedc4e32d2eda553791e3ffb893657bd2b0a75be
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-10 12:06:46 +0300

    the Lab Enum it's Done

---
+++ MyPlayEnum.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+enum Creatur {
+    
+    
+    case Human
+    case Vampire
+    case Werewolf
+}
+
+let MyCreatur = Creatur.Vampire
+
+switch MyCreatur {
+    
+case.Vampire:
+    
+print("I am a vampire")
+    
+case.Human:
+
+    print("I am Human")
+    
+case.Werewolf:
+    
+    print("I am a werewolf")
+}
+
+
